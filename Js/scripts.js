9b044115427401bc9f070a826f9bdc44be525e13
wcheung021 <wcheung021@gmail.com>
2024-02-26 22:32:14 +0000

    Favorite Food add in JavaScript

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,11 +1,6 @@
 alert('Hello world');
 
 
-let myName = 'Bob';
-console.log(myName);
-myName = 'John Doe';
-console.log(myName);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
+let favoriteFood = 'sushi';
+document.write(favoriteFood);
 
