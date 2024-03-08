4809190c7b546000ee8a82a5f912a0cf0248d23a
wcheung021 <wcheung021@gmail.com>
2024-03-08 23:54:53 +0000

    pokemon list weight update

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,7 +1,10 @@
 let pokemonList = [
-    {name: "charmander", heights: 0.6, type: "fire" , abilities: ["blaze", "solar-power"]},
-    {name: "charmeleon", heights: 1.1, type: "fire", abilities: ["blaze", "solar-power"]},
-    {name: "charizard", heights: 1.7, type: ["fire", "flying"], abilities: ["blaze", "solar-power"]}
+    {name: "charmander", heights: 0.6, weight: 8.5, type: "fire" , abilities: ["blaze", "solar-power"]},
+    {name: "charmeleon", heights: 1.1, weight: 19, type: "fire", abilities: ["blaze", "solar-power"]},
+    {name: "charizard", heights: 1.7, weight: 90.5, type: ["fire", "flying"], abilities: ["blaze", "solar-power"]},
+    {name: "pidgey", heights: 0.3, weight: 1.8, type: ["flying", "normal"], abilities: ["keen-eye", "tangled-feet", "big-pecks"]},
+    {name: "pidgeotto", heights: 1.1, weight: 30, type: ["flying", "normal"], abilities: ["keen-eye", "tangled-feet", "big-pecks"]},
+    {name:"pidgeot", heights: 1.5, weight: 39.5, type: ["flying", "normal"], abilities: ["keen-eye", "tangled-feet", "big-pecks"]},
 ];
 
 console.log(pokemonList);
