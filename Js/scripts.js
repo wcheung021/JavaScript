1ffbbdbfa09bc7d1ec77d8014ac4b3a14932bb7d
wcheung021 <wcheung021@gmail.com>
2024-03-08 23:40:44 +0000

    pokemon list upate

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,4 +1,7 @@
-let pokemonList = 
-    [{name: "charmander", heights: 0.6, type: "fire" , abilities: ["blaze", "solar-power"]}];
-    [{name: "charmeleon", heights: 1.1, type: "fire", abilities: ["blaze", "solar-power"]}];
-    [{name: "charizard", heights: 1.7, type: ["fire", "flying"], abilities: ["blaze", "solar-power"]}];
+let pokemonList = [
+    {name: "charmander", heights: 0.6, type: "fire" , abilities: ["blaze", "solar-power"]},
+    {name: "charmeleon", heights: 1.1, type: "fire", abilities: ["blaze", "solar-power"]},
+    {name: "charizard", heights: 1.7, type: ["fire", "flying"], abilities: ["blaze", "solar-power"]}
+];
+
+console.log(pokemonList);
