3a3d876cb0d291022343d49f81948d17158a7fdb
wcheung021 <wcheung021@gmail.com>
2024-03-18 02:23:59 +0000

    EX 1.7 need revise

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,97 +1,84 @@
 let pokemonRepository = (function () {
-    let repository = [
-      {
-          name: "charmander", 
-          height: 0.6, 
-          weight: 8.5, 
-          type: "fire" , 
-          abilities: ["blaze", "solar-power"]
-      },
-      {
-          name: "charmeleon", 
-          height: 1.1, 
-          weight: 19, 
-          type: "fire", 
-          abilities: ["blaze", "solar-power"]
-      },
-      {
-          name: "charizard", 
-          height: 1.7, 
-          weight: 90.5, 
-          type: ["fire", "flying"], 
-          abilities: ["blaze", "solar-power"]
-      },
-      {
-          name: "pidgey", 
-          height: 0.3, 
-          weight: 1.8, 
-          type: ["flying", "normal"], 
-          abilities: ["keen-eye", "tangled-feet", "big-pecks"]
-      },
-      {
-          name: "pidgeotto", 
-          height: 1.1, 
-          weight: 30, 
-          type: ["flying", "normal"], 
-          abilities: ["keen-eye", "tangled-feet", "big-pecks"]
-      },
-      {
-          name: "pidgeot", 
-          height: 1.5, 
-          weight: 39.5, 
-          type: ["flying", "normal"], 
-          abilities: ["keen-eye", "tangled-feet", "big-pecks"]
-      }
-    ];
+    let pokemonList = [];
+    let apiUrl= "https://pokeapi.co/api/v2/pokemon/?limit=150";
   
-    function add (pokemon){
-      if(
-          typeof pokemon === "object" &&
-          "name" in pokemon &&
-          "height" in pokemon &&
-          "weight" in pokemon &&
-          "type" in pokemon &&
-          "abilities" in pokemon
-      ){
-    
-        repository.push(pokemon);
-      }
+  function add (pokemon){
+    if (
+    typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "detailsUrl" in pokemon)
+    pokemonList.push(pokemon);
     }
   
     function getAll(){
-      return repository;
+      return pokemonList;
     }
     
     function addListItem(pokemon){
-        let pokemonList = document.querySelector(".pokemon-list");
-        let listpokemon = document.createElement("li");
-        let button = document.createElement("button");
-        button.innerText = pokemon.name;
-        button.classList.add("button-class");
-        listpokemon.appendChild(button);
-        pokemonList.appendChild(listpokemon);
-        button.addEventListener("click", function(event){
-            showDetails(pokemon);
+      let pokemonList = document.querySelector(".pokemon-list");
+      let listpokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+      button.addEventListener("click", function(event){
+        showDetails(pokemon);
+    });
+}
+    
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+        return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+            let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+    function loadDetails(pokemon){
+        let url = pokemon.detailsUrl;
+        return fetch(apiUrl).then(function(response){
+        return response.json();
+    }).then(function(details){
+        item.imageUrl = details.sprites.front_default;
+        item.name = details.name;
+        item.height = details.height;
+    }).catch(function(e){
+        console.log.error(e);
+    });
+    }
+
+    function showDetails(pokemon){
+        pokemonRepository.loadDetails(pokemon).then(function(){
+        console.log(pokemon);
         });
     }
 
-
-    function showDetails(pokemon){
-        console.log(pokemon);
-    }
     
     return{
         add: add,
         getAll: getAll,
-        addListItem: addListItem
+        addListItem: addListItem,
+        loadDetails: loadDetails,
+        showDetails: showDetails,
+        loadList: loadList
     };
-  })();
-  
-    pokemonRepository.add ({name: "arcanine", height: 1.9, weight: 155, type:"fire", abilities:["flashFire", "intimidate", "justified"]});
+})();
   
   
-    pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.loadList().then(function(){
+  pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
-    });
+  });
+  });
         
     
